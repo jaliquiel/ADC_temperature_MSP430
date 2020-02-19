278bf5c25ffc6f62f848318eff42f3e41c9bf72a
Alyssa Moore <abmoore810@gmail.com>
2020-02-19 10:54:34 -0500

    fixed getMonth, added defines, etc.

--- main.c
+++ main.c
@@ -11,21 +11,35 @@
  * into the main file of your project. */
 #include "peripherals.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
 
 // Function Prototypes
 void swDelay(char numLoops);
-void getMonth(unsigned int days, char *s);
-void displayTime(long unsigned int timer_cnt);
-void displayTemp(float inAvgTempC);
-void makeDate(long unsigned int month, long unsigned int days, char *s);
+//void getMonth(unsigned int days, char *s);
+//void displayTime(long unsigned int seconds);
+//void displayTemp(float inAvgTempC);
+//void makeDate(long unsigned int month, long unsigned int days, char *s);
 
 
 // Declare globals here
 long unsigned int timer_cnt = 0;
 int i;
+long unsigned int seconds;
 
+#define secondsInDay 86400
+#define secondsInHour 3600
+#define secondsInMinute 60
+#define minutesInHour 60
+#define hoursInDays 24
+#define daysInYear 365
 
+#define daysTilJanEnd 31
+#define daysTilFebEnd 31+28
+#define daysTilMarEnd 31+28+31
+#define daysTilAprEnd 31+28+31+30
 
 // Main
 void main(void)
@@ -44,102 +58,110 @@ void main(void)
     // *** Intro Screen ***
     Graphics_clearDisplay(&g_sContext); // Clear the display
 
+    seconds = 3500000;
 
     while (1)    // Forever loop
     {
-
-        displayTime(timer_cnt);
+        Graphics_clearDisplay(&g_sContext); // Clear the display
+        seconds++;
+        displayTime(seconds);
 
     }  // end while (1)
 }   //end main
 
-void getMonth(unsigned int days, char *s){
-    if(days <= 31){
+int getMonth(unsigned int *days, char *s){
+    if(days <= daysTilJanEnd){
         s[0] = 'J';
         s[1] = 'A';
         s[2] = 'N';
-    }else if(days > 31 && days <= 59){
+    }else if(days > daysTilJanEnd && days <= daysTilFebEnd){
         s[0] = 'F';
         s[1] = 'E';
         s[2] = 'B';
-    }else if(days > 59 && days <= 90){
+        days = days - daysTilJanEnd;
+    }else if(days > daysTilFebEnd && days <= daysTilMarEnd){
         s[0] = 'M';
         s[1] = 'A';
         s[2] = 'R';
-    }else if(days > 90 && days <= 120){
+        days = days - daysTilFebEnd;
+    }else if(days > daysTilMarEnd && days <= daysTilAprEnd){
         s[0] = 'A';
         s[1] = 'P';
         s[2] = 'R';
-    }else if(days > 120 && days <= 151){
+        days = days - daysTilMarEnd;
+    }else if(days > daysTilAprEnd && days <= 151){
         s[0] = 'M';
         s[1] = 'A';
         s[2] = 'Y';
+        days = days - daysTilAprEnd;
     }else if(days > 151 && days <= 181){
         s[0] = 'J';
         s[1] = 'U';
         s[2] = 'N';
+        days = days - 151;
     }else if(days > 181 && days <= 212){
         s[0] = 'J';
         s[1] = 'U';
         s[2] = 'L';
+        days = days - 181;
     }else if(days > 212 && days <= 243){
         s[0] = 'A';
         s[1] = 'U';
         s[2] = 'G';
+        days = days - 212;
     }else if(days > 243 && days <= 273){
         s[0] = 'S';
         s[1] = 'E';
         s[2] = 'P';
+        days = days - 243;
     }else if(days > 273 && days <= 304){
         s[0] = 'O';
         s[1] = 'C';
         s[2] = 'T';
+        days = days - 273;
     }else if(days > 304 && days <= 334){
         s[0] = 'N';
         s[1] = 'O';
         s[2] = 'V';
+        days = days - 304;
     }else if(days > 334 && days <= 365){
         s[0] = 'D';
         s[1] = 'E';
         s[2] = 'C';
+        days = days - 334;
     }else{
         days = 0;
     }
 }
 
-void displayTime(long unsigned int timer_cnt){
-    long unsigned int months, days, hours, min, sec, currentCount;
+void displayTime(long unsigned int seconds){
+    long unsigned int months, days, hours, min, sec, currSeconds;
     char time[9];
     char date[7];
 
     // save our current time
-    currentCount = timer_cnt;
+    currSeconds = seconds;
 
-    // 1 month ~ 87763132800 ticks
-    months = timer_cnt / 87763132800;    
+//    // 1 month ~ 87763132800 ticks
+//    months = timer_cnt / 87763132800;
 
-    // 1 day ~ 2831068800 ticks
-    days = (timer_cnt % 87763132800) / 2831068800; 
+    days = (int)(currSeconds / secondsInDay) % daysInYear;
 
-    // 1 hour ~ 117961200  ticks
-    hours = ((timer_cnt % 87763132800) % 2831068800) / 117961200; 
+    hours = (int)(currSeconds / secondsInHour) % hoursInDays;
  
-    // 1 minute ~ 1966020 ticks  
-    min = (((timer_cnt % 87763132800) % 2831068800) % 117961200) / 1966020;
-
-    // 1 second ~ 32767 ticks    
-    sec = ((((timer_cnt % 87763132800) % 2831068800) % 117961200) % 1966020) / 32767;
+    min = (int)(currSeconds / secondsInMinute) % minutesInHour;
    
+    sec = (int)(currSeconds % secondsInMinute);
+
     // Make strings
-    makeDate(months, days, date);
+    makeDate(days, date);
     makeTime(hours, min, sec, time);
 
-    char d[6] = "FEB 19";
     char t[8] = "08:45:00";
 
     // Display Strings
-    Graphics_drawStringCentered(&g_sContext, d, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, t, AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, date, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, t, AUTO_STRING_LENGTH, 48, 65, TRANSPARENT_TEXT);
     //refreshes display
     Graphics_flushBuffer(&g_sContext);
     
@@ -168,10 +190,13 @@ void displayTemp(float inAvgTempC){
 }
 
 // String functions
-void makeDate(long unsigned int month, long unsigned int days, char *s){
+void makeDate(long unsigned int days, char *s){
     // ..
-    getMonth(days, s);
-    char dayString[3];
+    getMonth(&days, s);
+    s[4] = ' ';
+    s[5] = days % 10 + '0';
+    s[6] = (int)(days / 10) % 10 + '0';
+    s[7] = '\0';
 }
 
 void makeTime(long unsigned int hours, long unsigned int min, long unsigned int sec, char *s)
