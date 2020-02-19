3b0a145eb4ec428b48ada78f7729b5ff3a91fb7b
Alyssa Moore <abmoore810@gmail.com>
2020-02-19 09:24:41 -0500

    solved problems in getMonth

--- main.c
+++ main.c
@@ -10,13 +10,22 @@
  * You include the header associated with that file(s)
  * into the main file of your project. */
 #include "peripherals.h"
+#include <stdio.h>
+#include <string.h>
 
 // Function Prototypes
 void swDelay(char numLoops);
+void getMonth(unsigned int days, char *s);
+void displayTime(long unsigned int timer_cnt);
+void displayTemp(float inAvgTempC);
+void makeDate(long unsigned int month, long unsigned int days, char *s);
+
 
 // Declare globals here
 long unsigned int timer_cnt = 0;
 int i;
+
+
 
 // Main
 void main(void)
@@ -35,55 +44,73 @@ void main(void)
     // *** Intro Screen ***
     Graphics_clearDisplay(&g_sContext); // Clear the display
 
-    // Write some text to the display
-    Graphics_drawStringCentered(&g_sContext, "Welcome", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "to", AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "ECE2049-C20!", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
-    //refreshes display
-    Graphics_flushBuffer(&g_sContext);
 
     while (1)    // Forever loop
     {
 
-
+        displayTime(timer_cnt);
 
     }  // end while (1)
 }   //end main
 
-void getMonth(unsigned int days){
-    if(days <= 31)
-        monthString = "JAN";
-    if(days > 31 && days <= 59)
-        monthString = "FEB";
-    if(days > 59 && days <= 90)
-        monthString = "MAR";
-    if(days > 90 && days <= 120)
-        monthString = "APR";
-    if(days > 120 && days <= 151)
-        monthString = "MAY";
-    if(days > 151 && days <= 181)
-        monthString = "JUN";
-    if(days > 181 && days <= 212)
-        monthString = "JUL";
-    if(days > 212 && days <= 243)
-        monthString = "AUG";
-    if(days > 243 && days <= 273)
-        monthString = "SEP";
-    if(days > 273 && days <= 304)
-        monthString = "OCT";
-    if(days > 304 && days <= 334)
-        monthString = "NOV";
-    if(days > 334 && days <= 365)
-        monthString = "DEC";
-    else
+void getMonth(unsigned int days, char *s){
+    if(days <= 31){
+        s[0] = 'J';
+        s[1] = 'A';
+        s[2] = 'N';
+    }else if(days > 31 && days <= 59){
+        s[0] = 'F';
+        s[1] = 'E';
+        s[2] = 'B';
+    }else if(days > 59 && days <= 90){
+        s[0] = 'M';
+        s[1] = 'A';
+        s[2] = 'R';
+    }else if(days > 90 && days <= 120){
+        s[0] = 'A';
+        s[1] = 'P';
+        s[2] = 'R';
+    }else if(days > 120 && days <= 151){
+        s[0] = 'M';
+        s[1] = 'A';
+        s[2] = 'Y';
+    }else if(days > 151 && days <= 181){
+        s[0] = 'J';
+        s[1] = 'U';
+        s[2] = 'N';
+    }else if(days > 181 && days <= 212){
+        s[0] = 'J';
+        s[1] = 'U';
+        s[2] = 'L';
+    }else if(days > 212 && days <= 243){
+        s[0] = 'A';
+        s[1] = 'U';
+        s[2] = 'G';
+    }else if(days > 243 && days <= 273){
+        s[0] = 'S';
+        s[1] = 'E';
+        s[2] = 'P';
+    }else if(days > 273 && days <= 304){
+        s[0] = 'O';
+        s[1] = 'C';
+        s[2] = 'T';
+    }else if(days > 304 && days <= 334){
+        s[0] = 'N';
+        s[1] = 'O';
+        s[2] = 'V';
+    }else if(days > 334 && days <= 365){
+        s[0] = 'D';
+        s[1] = 'E';
+        s[2] = 'C';
+    }else{
         days = 0;
+    }
 }
 
-void displayTime(long unsigned int timer_cnt)
-{
+void displayTime(long unsigned int timer_cnt){
     long unsigned int months, days, hours, min, sec, currentCount;
-    char[9] time;
-    char[7] date;
+    char time[9];
+    char date[7];
 
     // save our current time
     currentCount = timer_cnt;
@@ -107,16 +134,20 @@ void displayTime(long unsigned int timer_cnt)
     makeDate(months, days, date);
     makeTime(hours, min, sec, time);
 
+    char d[6] = "FEB 19";
+    char t[8] = "08:45:00";
+
     // Display Strings
-    Graphics_drawStringCentered(&g_sContext, date, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, time, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, d, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, t, AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
+    //refreshes display
+    Graphics_flushBuffer(&g_sContext);
     
 }
 
-void displayTemp(float inAvgTempC)
-{
-    char[8] cString;
-    char[8] fString;
+void displayTemp(float inAvgTempC){
+    char cString[8];
+    char fString[8];
 
     // get copy of float
     float celciusTemp = inAvgTempC;
@@ -126,28 +157,29 @@ void displayTemp(float inAvgTempC)
     
     // Make strings
     makeTemp(celciusTemp, cString, true);
-    makeTemp(farenTemp, fString, false);
+    makeTemp(fahrenTemp, fString, false);
 
     // Display Strings
     Graphics_drawStringCentered(&g_sContext, cString, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, fString, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, fString, AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
+    //refreshes display
+    Graphics_flushBuffer(&g_sContext);
 
 }
 
 // String functions
-void makeDate(long unsigned int month, long unsigned int day, char *s)
-{
+void makeDate(long unsigned int month, long unsigned int days, char *s){
     // ..
-    char[3] monthString;
-    char[3] dayString;
+    getMonth(days, s);
+    char dayString[3];
 }
 
 void makeTime(long unsigned int hours, long unsigned int min, long unsigned int sec, char *s)
 {
     // ..
-    char[3] hourString;
-    char[3] minString;
-    char[3] secString;
+    char hourString[3];
+    char minString[3];
+    char secString[3];
 }
 
 void makeTemp(float temperature, char * s, bool isCelsius)
