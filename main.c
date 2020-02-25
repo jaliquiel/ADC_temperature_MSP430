8154ef03d87624d86065db09e2cc7bc1f3a4b2d3
Alyssa Moore <abmoore810@gmail.com>
2020-02-24 19:24:47 -0500

    tried to fix getMonth function

--- main.c
+++ main.c
@@ -37,9 +37,9 @@ long unsigned int seconds;
 #define daysInYear 365
 
 #define daysTilJanEnd 31
-#define daysTilFebEnd 31+28
-#define daysTilMarEnd 31+28+31
-#define daysTilAprEnd 31+28+31+30
+#define daysTilFebEnd 59
+#define daysTilMarEnd 90
+#define daysTilAprEnd 120
 
 // Temperature Sensor Calibration = Reading at 30 degrees C is stored at addr 1A1Ah
 // See end of datasheet for TLV table memory mapping
@@ -70,107 +70,46 @@ void main(void)
         Graphics_clearDisplay(&g_sContext); // Clear the display
         seconds++;
         displayTime(seconds);
-        displayTemp(in_temp);
+        //displayTemp(in_temp);
 
     }  // end while (1)
 }   //end main
 
-int getMonth(unsigned int *days, char *s){
-    if(days <= daysTilJanEnd){
-        s[0] = 'J';
-        s[1] = 'A';
-        s[2] = 'N';
-    }else if(days > daysTilJanEnd && days <= daysTilFebEnd){
-        s[0] = 'F';
-        s[1] = 'E';
-        s[2] = 'B';
-        days = days - daysTilJanEnd;
-    }else if(days > daysTilFebEnd && days <= daysTilMarEnd){
-        s[0] = 'M';
-        s[1] = 'A';
-        s[2] = 'R';
-        days = days - daysTilFebEnd;
-    }else if(days > daysTilMarEnd && days <= daysTilAprEnd){
-        s[0] = 'A';
-        s[1] = 'P';
-        s[2] = 'R';
-        days = days - daysTilMarEnd;
-    }else if(days > daysTilAprEnd && days <= 151){
-        s[0] = 'M';
-        s[1] = 'A';
-        s[2] = 'Y';
-        days = days - daysTilAprEnd;
-    }else if(days > 151 && days <= 181){
-        s[0] = 'J';
-        s[1] = 'U';
-        s[2] = 'N';
-        days = days - 151;
-    }else if(days > 181 && days <= 212){
-        s[0] = 'J';
-        s[1] = 'U';
-        s[2] = 'L';
-        days = days - 181;
-    }else if(days > 212 && days <= 243){
-        s[0] = 'A';
-        s[1] = 'U';
-        s[2] = 'G';
-        days = days - 212;
-    }else if(days > 243 && days <= 273){
-        s[0] = 'S';
-        s[1] = 'E';
-        s[2] = 'P';
-        days = days - 243;
-    }else if(days > 273 && days <= 304){
-        s[0] = 'O';
-        s[1] = 'C';
-        s[2] = 'T';
-        days = days - 273;
-    }else if(days > 304 && days <= 334){
-        s[0] = 'N';
-        s[1] = 'O';
-        s[2] = 'V';
-        days = days - 304;
-    }else if(days > 334 && days <= 365){
-        s[0] = 'D';
-        s[1] = 'E';
-        s[2] = 'C';
-        days = days - 334;
-    }else{
-        days = 0;
-    }
-}
-
 void displayTime(long unsigned int seconds){
-    long unsigned int months, days, hours, min, sec, currSeconds;
+    long unsigned int days, hours, min, sec, currSeconds;
     char time[9];
-    char date[7];
+    char *date[7];
 
+    runTimerA2();
+
+    unsigned int elapsedSec = timer_cnt;
     // save our current time
     currSeconds = seconds;
-
-//    // 1 month ~ 87763132800 ticks
-//    months = timer_cnt / 87763132800;
 
     days = (int)(currSeconds / secondsInDay) % daysInYear;
 
     hours = (int)(currSeconds / secondsInHour) % hoursInDays;
- 
-    min = (int)(currSeconds / secondsInMinute) % minutesInHour;
-   
+
+    min = (int)(currSeconds / secondsInHour) % minutesInHour;
+
     sec = (int)(currSeconds % secondsInMinute);
 
     // Make strings
-    makeDate(days, date);
+    getMonth(days, date);
+    //makeDate(days, date);
     makeTime(hours, min, sec, time);
 
-    char t[8] = "08:45:00";
+    char a[7] = "AUG 10\0";
+    char t[8] = "08:45:00\0";
 
     // Display Strings
-    Graphics_drawStringCentered(&g_sContext, date, AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, t, AUTO_STRING_LENGTH, 48, 65, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, a, AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, t, AUTO_STRING_LENGTH, 48, 45, TRANSPARENT_TEXT);
     //refreshes display
-    Graphics_flushBuffer(&g_sContext);
-    
+    if(elapsedSec >= 3){
+        Graphics_flushBuffer(&g_sContext);
+        stopTimerA2(1);
+    }
 }
 
 void displayTemp(float inAvgTempC){
@@ -185,7 +124,7 @@ void displayTemp(float inAvgTempC){
 
     // Convert celcius to fahrenheit
     float fahrenTemp = 1.8 * (celciusTemp) + 32;
-    
+
     // Make strings
     makeTemp(celciusTemp, cString, true);
     makeTemp(fahrenTemp, fString, false);
@@ -198,39 +137,106 @@ void displayTemp(float inAvgTempC){
 
 }
 
-// String functions
-void makeDate(long unsigned int days, char *s){
-    // ..
-    getMonth(&days, s);
-    s[4] = ' ';
-    s[5] = days % 10 + '0';
-    s[6] = (int)(days / 10) % 10 + '0';
-    s[7] = '\0';
+char getMonth(unsigned int days, char *date){
+    if(days <= daysTilJanEnd){
+        date[0] = 'J';
+        date[1] = 'A';
+        date[2] = 'N';
+    }else if(days > daysTilJanEnd && days <= daysTilFebEnd){
+        date[0] = 'F';
+        date[1] = 'E';
+        date[2] = 'B';
+        days = days - daysTilJanEnd;
+    }else if(days > daysTilFebEnd && days <= daysTilMarEnd){
+        date[0] = 'M';
+        date[1] = 'A';
+        date[2] = 'R';
+        days = days - daysTilFebEnd;
+    }else if(days > daysTilMarEnd && days <= daysTilAprEnd){
+        date[0] = 'A';
+        date[1] = 'P';
+        date[2] = 'R';
+        days = days - daysTilMarEnd;
+    }else if(days > daysTilAprEnd && days <= 151){
+        date[0] = 'M';
+        date[1] = 'A';
+        date[2] = 'Y';
+        days = days - daysTilAprEnd;
+    }else if(days > 151 && days <= 181){
+        date[0] = 'J';
+        date[1] = 'U';
+        date[2] = 'N';
+        days = days - 151;
+    }else if(days > 181 && days <= 212){
+        date[0] = 'J';
+        date[1] = 'U';
+        date[2] = 'L';
+        days = days - 181;
+    }else if(days > 212 && days <= 243){
+        date[0] = 'A';
+        date[1] = 'U';
+        date[2] = 'G';
+        days = days - 212;
+    }else if(days > 243 && days <= 273){
+        date[0] = 'S';
+        date[1] = 'E';
+        date[2] = 'P';
+        days = days - 243;
+    }else if(days > 273 && days <= 304){
+        date[0] = 'O';
+        date[1] = 'C';
+        date[2] = 'T';
+        days = days - 273;
+    }else if(days > 304 && days <= 334){
+        date[0] = 'N';
+        date[1] = 'O';
+        date[2] = 'V';
+        days = days - 304;
+    }else if(days > 334 && days <= 365){
+        date[0] = 'D';
+        date[1] = 'E';
+        date[2] = 'C';
+        days = days - 334;
+    }else{
+        days = 0;
+    }
+    date[3] = ' ';
+    date[4] = (int)(days / 10) % 10 + '0';
+    date[5] = days % 10 + '0';
+    date[6] = '\0';
+    return date;
 }
+
+// String functions
+//void makeDate(long unsigned int days, char *date[7]){
+//    date[3] = ' ';
+//    date[4] = (int)(days / 10) % 10 + '0';
+//    date[5] = days % 10 + '0';
+//    date[6] = '\0';
+//}
 
 void makeTime(long unsigned int hours, long unsigned int min, long unsigned int sec, char *s)
 {
-    // ..
     char hourString[3];
     char minString[3];
     char secString[3];
 }
 
-void makeTemp(float temperature, char * s, bool isCelsius)
-{
-    s[0] = (int)(temperature / 100.0);
-    s[1] = (int)((temperature % 100.0) / 10.0);
-    s[2] = (int)(temperature % 10.0);
-    s[3] = '.';
-    s[4] = (int)((temperature * 10.0) % 10.0);
-    s[5] = ' ';
-    if(isCelcius){
-        s[6] = 'C';
-    }else{
-        s[6] = 'F';
-    }
-    s[7] = '\0';
-}
+//void makeTemp(float temperature, char * s, bool isCelsius)
+//{
+//    s[0] = (floor((double)temperature / 100.0));
+//    s[1] = (floor((double)((int)temperature % 100.0) / 10.0));
+//    s[2] = (floor((double)((int)temperature % 10.0));
+//    s[3] = '.';
+//    s[4] = (floor(((double)temperature * 10.0) % 10.0));
+//    s[5] = ' ';
+//    if(isCelsius == true){
+//        s[6] = 'C';
+//    }else{
+//        s[6] = 'F';
+//    }
+//    s[7] = '\0';
+//}
 
 float configADC(void){
     volatile float temperatureDegC;
@@ -297,7 +303,7 @@ void swDelay(char numLoops)
     }
 }
 
-void runTimerA2(){
+void runTimerA2(void){
     TA2CTL = TASSEL_1 + ID_0 + MC_1;
     TA2CCR0 = 32767; // interrupt every 1 second
     TA2CCTL0 = CCIE;
