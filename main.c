24c37ebefb343cb38e32ea492c60d5807931bf38
abmoore810 <59891541+abmoore810@users.noreply.github.com>
2020-02-24 23:31:37 -0500

    Update main.c
    added current sensor to adc config and states

--- main.c
+++ main.c
@@ -57,8 +57,13 @@ char date[7] = {0};
 #define CALADC12_15V_30C  *((unsigned int *)0x1A1A)
 // Temperature Sensor Calibration = Reading at 85 degrees C is stored at addr 1A1Ch
 #define CALADC12_15V_85C  *((unsigned int *)0x1A1C)
+// Resolution for current sensor:  1.0A/4096
+// (Here, we add the f suffix so the compiler will know this constant is a float)
+#define MA_PER_BIT        (0.0244f)
 
-unsigned int in_temp;
+unsigned int in_temp, in_current;
+
+enum LCD_STATE {display = 0, edit = 1};
 
 // Main
 void main(void)
@@ -73,12 +78,15 @@ void main(void)
     configDisplay();
     configKeypad();
 
+    enum LCD_STATE state = display;
+	
     // set up temperature sensor *****************************************************
     float temperatureAvg [36] = {0};
     int tempAvgIndex = 0;
     int tempIndex = 0;
     volatile float temperatureDegC;
     volatile float temperatureDegF;
+    volatile float milliamps;
     volatile float degC_per_bit;
     volatile unsigned int bits30, bits85;
 
@@ -86,12 +94,14 @@ void main(void)
                             // internal reference voltages to
                             // ADC12_A control registers
 //    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;     // Internal ref = 1.5V (TODOOOOOOOO UNCOMMENT LATEEER)
-    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON;     // Internal ref = 1.5V
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;     // Internal ref = 1.5V
 
-    ADC12CTL1 = ADC12SHP;                     // Enable sample timer
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;                     // Enable sample timer
     // Using ADC12MEM0 to store reading
-    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_0;  // ADC i/p ch A0 = current sense
                                               // ACD12SREF_1 = internal ref = 1.5v
+    ADC12MCTL1 = ADC12SREF_1 | ADC12INCH_10 + ADC12EOS; // ADC i/p ch A10 = temp sense
+    P6SEL |= BIT0; 
     __delay_cycles(100);                    // delay to allow Ref to settle
     ADC12CTL0 |= ADC12ENC;              // Enable conversion
     // Use calibration data stored in info memory
@@ -124,7 +134,8 @@ void main(void)
             // Poll busy bit waiting for conversion to complete
             while (ADC12CTL1 & ADC12BUSY)
                 __no_operation();
-            in_temp = ADC12MEM0;      // Read in results if conversion
+	    in_current = ADC12MEM0;
+            in_temp = ADC12MEM1;      // Read in results if conversion
 
             // Temperature in Celsius. See the Device Descriptor Table section in the
             // System Resets, Interrupts, and Operating Modes, System Control Module
@@ -132,6 +143,8 @@ void main(void)
             // formula.
             temperatureDegC = (float)((long)in_temp - CALADC12_15V_30C) * degC_per_bit +30.0;
 
+	    milliamps = ((float)in_current) * MA_PER_BIT;
+		
             // Temperature in Fahrenheit
             //temperatureDegF = (temperatureDegC * 1.8) + 32;  //conversion formula for celcius to fahrenheit
             __no_operation();
